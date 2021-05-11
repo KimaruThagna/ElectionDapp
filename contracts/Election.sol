a8e04c8f1608ba76ca041964f32afc1e1bcfb5fb
Kimaru thagana <macbuntu@localhost.localdomain>
2021-05-11 10:19:19 +0300

    make elections contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,25 @@
 pragma solidity >=0.4.22 <0.8.0;
 
 contract Election {
-        string public candidate;
+    // special data type to hold a candidate
+    struct Candidate{
+        uint id;
+        string politicalParty;
+        string name;
+        uint voteCount;
+    }
+    // keep track of number of candidates
+    uint public candidatesCount;
+    //fetch candidate by id. Consider it a dictionary of candidate objects in Python
+    mapping(uint => Candidate) public candidates;
     // Constructor. Declare public since its gonna be run on deployment
     constructor () public {
-            candidate = "Raila";
+            addCandidate("Raila","One last chance")
+            addCandidate("Ruto","Im not like other guys")
+            addCandidate("Jayden","Suffering")
+    }
+    function addCandidate(string _name,string _party) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount,_party,_name,0);
     }
 }
